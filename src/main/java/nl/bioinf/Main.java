a1f474c26e7bcc12c3446f334621f695e7f4a4ac
Iris_Ineke <i.e.ineke@st.hanze.nl>
2025-10-02 13:26:23 +0200

    dummy classes staan nu in uml, maar lijntjes ertussen ontbreken nog...

--- src/main/java/nl/bioinf/Main.java
+++ src/main/java/nl/bioinf/Main.java
@@ -1,13 +1,17 @@
 package nl.bioinf;
 
-
 public class Main {
+
+
     public static void main(String[] args) {
         int exitCode = new picocli.CommandLine(new ArgumentParser()).execute(args);
-        System.exit(exitCode); // geef mee aan het systeem of het goed gaat (0 of 1)
-//      System.exit(new CommandLine(new MyApp()).execute(args));
-        }
 
+        OutputGenerator generator = new OutputGenerator();
+        InteractionChecker checker = new InteractionChecker();
+
+
+        System.exit(exitCode);
     }
+}
 
 
