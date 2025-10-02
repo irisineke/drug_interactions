aef6ed6d2c7b68bc713a4adb0468cb877deb52d9
Iris_Ineke <i.e.ineke@st.hanze.nl>
2025-10-02 15:44:28 +0200

    basis params zijn er nu, fileNotEmptyCheck ook erbij

--- src/main/java/nl/bioinf/ArgumentParser.java
+++ src/main/java/nl/bioinf/ArgumentParser.java
@@ -17,17 +17,42 @@ import java.util.List;
 // Callable: Integer of wat anders teruggeven ?
 public class ArgumentParser implements Runnable {
 //    vangt input file op
-    @Option(names = { "-f", "--file" },
-            paramLabel = "inputFile", // zo heet hij in help
-            description = "the input file", // ook in help
-            required = false)
-    File inputFile;
+    @Option(names = { "-intF", "--interactionsFile" },
+            paramLabel = "interactionsFile", // zo heet hij in help
+            description = "the input file: interactions.tsv", // ook in help
+            required = true)
+    File interactionsFile;
+
+    @Option(names = { "-drF", "--drugsFile" },
+            paramLabel = "drugsFile", // zo heet hij in help
+            description = "the input file: drugs.tsv", // ook in help
+            required = true)
+    File drugsFile;
+
+    @Option(names = {"-drug1", "-d1"},
+            paramLabel = "firstDrugInput",
+            description = "put the first drug you want to compare here",
+            required = true)
+    File firstDrugInput;
+
+    @Option(names = {"-drug2", "-d2"},
+            paramLabel = "secondDrugInput",
+            description = "put the second drug you want to compare here",
+            required = true)
+    File secondDrugInput;
 
 
     @Override
     public void run() {
+
         try {
+            // roept de fileNotEmptyCheck aan met de files als input
+            fileNotEmptyCheck("Interactions file", interactionsFile.getAbsolutePath());
+            fileNotEmptyCheck("Drugs file", drugsFile.getAbsolutePath());
+
+
             prepareAndPrintData();
+
         } catch (Exception e) {
             System.err.println("❌ FOUT: " + e.getMessage());
             e.printStackTrace();
@@ -35,14 +60,36 @@ public class ArgumentParser implements Runnable {
         }
     }
 
-    private void prepareAndPrintData() throws Exception {
-//      checkt of de inputFile uit de commandline is meegegeven
-        Path rawInteractions = inputFile != null ? inputFile.toPath()
-                : Path.of("data/raw/interactions.tsv");
-        Path rawDrugs = Path.of("data/raw/drugs.tsv");
 
+    public void fileNotEmptyCheck(String name, String value){
+        // verwijder deze regel niet nog een keer per ongeluk pls !!!
+        File file = new File(value);
+
+        // checken of opgegeven file bestaat
+        if (!file.exists()) {
+            throw new IllegalArgumentException("❌ FOUT: " + name + " does not exist :(");
+        }
+        // checken of opgegeven file daadwerklijk een file is (super handig) (ipv directory bv)
+        if (!file.isFile()){
+            throw new IllegalArgumentException("❌ FOUT: " + name + " is not a file :(");
+        }
+        // checken of file niet leeg is
+            // ! moest dit nou met == of .equals ?
+        if (file.length() == 0) {
+            throw new IllegalArgumentException("❌ FOUT: " + name + " is empty :(");
+        }
+    }
+
+
+    private void prepareAndPrintData() throws Exception {
+        // de paden uit de input gebruiken als raw bestanden
+        Path rawInteractions = interactionsFile.toPath();
+        Path rawDrugs = drugsFile.toPath();
+
+       // dot wordt dus nog aangepast als we in een class tijdelijk op gaan slaan !
         Path preparedInteractions = Path.of("data/prepared/interactions.tsv");
         Path preparedDrugs = Path.of("data/prepared/drugs.tsv");
+
 
         List<String> keepInteractions = List.of(
                 "gene_claim_name",
@@ -72,13 +119,11 @@ public class ArgumentParser implements Runnable {
         System.out.println("\nVoorbeeld (eerste 2 regels drugs.tsv):");
         LeesBestanden.printFirstNLines(preparedDrugs, 2);
     }
+
+//    tijdleijk voor uml
     OutputGenerator generator = new OutputGenerator();
     InteractionChecker checker = new InteractionChecker();
 
-//    public static void main(String[] args) {
-//        int exitCode = new CommandLine(new ArgumentParser()).execute(args);
-//        System.exit(exitCode);
-//    }
 }
 // maakt commandline object van de class argumentparser
 //    public static void main(String[] args) {
@@ -89,4 +134,4 @@ public class ArgumentParser implements Runnable {
 
 
 
-// runnen: ./gradlew run --args="--file data/raw/interactions.tsv"
+// runnen: ./gradlew run --args='-intF data/raw/interactions.tsv -drF data/raw/drugs.tsv -d1 a -d2 b'
--- src/main/java/nl/bioinf/Main.java
+++ src/main/java/nl/bioinf/Main.java
@@ -1,13 +1,8 @@
 package nl.bioinf;
 
 public class Main {
-
-
     public static void main(String[] args) {
         int exitCode = new picocli.CommandLine(new ArgumentParser()).execute(args);
-
-
-
         System.exit(exitCode);
     }
 }
